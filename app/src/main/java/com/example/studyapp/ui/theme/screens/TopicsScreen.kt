b18086e604484b1ef8d5847315a0a96ae380fbdb
Laura Voetzsch <lauravoetzsch@gmail.com>
2024-10-09 17:56:52 +0200

    Implement TopicsScreen
    Signed-off-by: Laura Voetzsch <lauravoetzsch@gmail.com>

--- app/src/main/java/com/example/studyapp/ui/theme/screens/TopicsScreen.kt
+++ app/src/main/java/com/example/studyapp/ui/theme/screens/TopicsScreen.kt
@@ -1,15 +1,33 @@
 package com.example.studyapp.ui.theme.screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Create
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,26 +46,124 @@ fun TopicsScreen(modifier: Modifier = Modifier) {
                     Icon(
                         imageVector = Icons.Default.Search,
                         contentDescription = stringResource(R.string.topics_search),
-                        modifier = Modifier.padding(start = 16.dp, end = 24.dp)
+                        modifier = Modifier.padding(start = 16.dp)
                     )
                 },
                 actions = {
-                    Icon(
-                        imageVector = Icons.Default.MoreVert,
-                        contentDescription = stringResource(R.string.menu),
-                        modifier = Modifier.padding(start = 24.dp, end = 16.dp)
-                    )
+                    MoreActionsMenu(
+                        Modifier.padding(end = 16.dp)
+                    ) // TODO Check whether padding is correct
                 },
                 title = {
                     Text(text = stringResource(R.string.app_name))
                 },
             )
-
+        },
+        floatingActionButton = {
+            CreateTopicFAB(
+                onCreate = { /* TODO Implement */ }
+            )
         }
     ) { innerPadding ->
+        LazyColumn(Modifier.padding(innerPadding)) {
+
+        }
     }
 
 }
+
+@Composable
+private fun MoreActionsMenu(modifier: Modifier = Modifier) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    Column(modifier, horizontalAlignment = Alignment.End) {
+        Icon(
+            imageVector = Icons.Default.MoreVert,
+            contentDescription = stringResource(R.string.menu),
+            modifier = Modifier.clickable { expanded = true }
+        )
+
+        DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+            DropdownMenuItem(
+                text = { Text(stringResource(R.string.share)) },
+                onClick = { /* TODO Implement */ },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Outlined.Share,
+                        contentDescription = null
+                    )
+                }
+            )
+            HorizontalDivider()
+            DropdownMenuItem(
+                text = { Text(stringResource(R.string.delete)) },
+                onClick = { /* TODO Implement */ },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Outlined.Delete,
+                        contentDescription = null
+                    )
+                }
+            )
+        }
+    }
+}
+
+@Composable
+private fun CreateTopicFAB(onCreate: (String) -> Unit, modifier: Modifier = Modifier) {
+    var showDialog by rememberSaveable { mutableStateOf(false) }
+    FloatingActionButton(onClick = { showDialog = true }, modifier = modifier) {
+        Icon(
+            imageVector = Icons.Outlined.Create,
+            contentDescription = stringResource(R.string.create)
+        )
+    }
+    if (showDialog) {
+        CreateTopicDialog(
+            onDismiss = { showDialog = false },
+            onCreate = onCreate
+        )
+    }
+}
+
+@Composable
+private fun CreateTopicDialog(
+    modifier: Modifier = Modifier,
+    onDismiss: () -> Unit = { },
+    onCreate: (String) -> Unit = { }
+) {
+    var topicTitle by rememberSaveable { mutableStateOf("") }
+
+    AlertDialog(
+        onDismissRequest = { onDismiss() },
+        title = { Text(stringResource(R.string.create_topic)) },
+        text = {
+            TextField(
+                value = topicTitle,
+                onValueChange = { topicTitle = it }, // Update the state with new value
+                label = { Text(stringResource(R.string.title)) }
+            )
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onCreate(topicTitle)
+                    onDismiss()
+                }
+            ) {
+                Text(stringResource(R.string.create))
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = { onDismiss() }
+            ) {
+                Text(stringResource(R.string.cancel))
+            }
+        },
+        modifier = modifier
+    )
+}
+
 
 @Preview
 @Composable
