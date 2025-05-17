8db9eb2657bd2757951c74b8578052e684bcbcd9
lauravoetzsch <LauraVoetzsch@gmail.com>
2025-05-17 11:20:14 +0200

    Use LargeFlexibleTopAppBar in SubtopicScaffold
    Signed-off-by: lauravoetzsch <LauraVoetzsch@gmail.com>

--- app/src/main/java/com/example/studyapp/ui/screens/subtopic/SubtopicScaffold.kt
+++ app/src/main/java/com/example/studyapp/ui/screens/subtopic/SubtopicScaffold.kt
@@ -11,10 +11,11 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.LargeFlexibleTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -43,7 +44,10 @@ private enum class DialogType {
     DELETE_SUBTOPIC
 }
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3AdaptiveApi::class)
+@OptIn(
+    ExperimentalMaterial3Api::class, ExperimentalMaterial3AdaptiveApi::class,
+    ExperimentalMaterial3ExpressiveApi::class
+)
 @Composable
 fun SubtopicScaffold(
     subtopic: Subtopic,
@@ -87,7 +91,7 @@ fun SubtopicScaffold(
         Scaffold(
             modifier = modifier,
             topBar = {
-                LargeTopAppBar(
+                LargeFlexibleTopAppBar(
                     title = {
                         Text(
                             text = subtopic.title,
