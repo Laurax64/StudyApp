e4608b6e5cd33f977ee369c0559e116999dff36b
lauravoetzsch <LauraVoetzsch@gmail.com>
2025-06-07 19:10:09 +0200

    Add GetTopicsUseCase
    Signed-off-by: lauravoetzsch <LauraVoetzsch@gmail.com>

--- app/src/main/java/com/example/studyapp/ui/topics/TopicsViewModel.kt
+++ app/src/main/java/com/example/studyapp/ui/topics/TopicsViewModel.kt
@@ -1,10 +1,10 @@
 package com.example.studyapp.ui.topics
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.studyapp.data.Topic
 import com.example.studyapp.data.TopicsRepository
+import com.example.studyapp.domain.GetTopicsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
@@ -13,20 +13,19 @@ import javax.inject.Inject
 
 @HiltViewModel
 class TopicsViewModel @Inject constructor(
-    private val topicsRepository: TopicsRepository,
+    getTopicsWithProgress: GetTopicsUseCase,
+    private val topicsRepository: TopicsRepository
 ) : ViewModel() {
-    val topics = topicsRepository.getAllTopics()
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.Companion.WhileSubscribed(5_000),
-            initialValue = null
-        )
+    val topics = getTopicsWithProgress().stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = null
+    )
 
     fun saveTopic(topic: Topic) {
         viewModelScope.launch {
-            Log.d("TopicsViewModel", "Saving topic: $topic")
             topicsRepository.insertTopic(topic = topic)
-            Log.d("TopicsViewModel", "Topic saved")
         }
     }
 }
+
