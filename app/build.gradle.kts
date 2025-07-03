34fd976d334fa290153cf6f4e5bd324b089942e6
lauravoetzsch <LauraVoetzsch@gmail.com>
2025-07-03 12:47:51 +0200

    Upgrade android-junit5 plugin to version catalog
    Signed-off-by: lauravoetzsch <LauraVoetzsch@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.ksp)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kotlin.serialization)
-    id("de.mannodermaus.android-junit5") version "1.13.1.0"
+    alias(libs.plugins.android.junit5)
 }
 
 android {
