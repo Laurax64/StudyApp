925e383d1b1afecff0cd6282af683cf9ef5ff6df
lauravoetzsch <LauraVoetzsch@gmail.com>
2025-07-05 19:30:27 +0200

    Add `DeviceConfigurationOverride` for testing different screen sizes
    Signed-off-by: lauravoetzsch <LauraVoetzsch@gmail.com>

---
+++ app/src/androidTest/java/com/example/studyapp/DeviceConfigurationOverride.kt
@@ -0,0 +1,82 @@
+package com.example.studyapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.test.DeviceConfigurationOverride
+import androidx.compose.ui.test.ForcedSize
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.window.core.layout.WindowSizeClass.Companion.HEIGHT_DP_MEDIUM_LOWER_BOUND
+import androidx.window.core.layout.WindowSizeClass.Companion.WIDTH_DP_EXPANDED_LOWER_BOUND
+import androidx.window.core.layout.WindowSizeClass.Companion.WIDTH_DP_MEDIUM_LOWER_BOUND
+
+enum class DeviceSize(
+    val dpSize: DpSize
+) {
+    COMPACT_WIDTH_EXPANDED_HEIGHT(
+        dpSize = DpSize(
+            width = (WIDTH_DP_MEDIUM_LOWER_BOUND - 1).dp,
+            height = WIDTH_DP_EXPANDED_LOWER_BOUND.dp
+        )
+    ),
+    MEDIUM_WIDTH_EXPANDED_HEIGHT(
+        dpSize = DpSize(
+            width = WIDTH_DP_EXPANDED_LOWER_BOUND.dp,
+            height = WIDTH_DP_EXPANDED_LOWER_BOUND.dp
+        )
+    ),
+    EXPANDED_WIDTH_COMPACT_HEIGHT(
+        dpSize = DpSize(
+            width = WIDTH_DP_EXPANDED_LOWER_BOUND.dp,
+            height = (HEIGHT_DP_MEDIUM_LOWER_BOUND - 1).dp
+        )
+    )
+}
+
+@Composable
+fun DeviceConfigurationOverride(
+    deviceSize: DeviceSize,
+    content: @Composable () -> Unit
+) {
+    DeviceConfigurationOverride(
+        override = DeviceConfigurationOverride.ForcedSize(
+            size = deviceSize.dpSize
+        )
+    )
+    {
+        content()
+    }
+}
+
+
+@Composable
+fun DeviceConfigurationOverride.MediumWidthExpandedHeight(
+    content: @Composable () -> Unit
+) {
+    DeviceConfigurationOverride(
+        override = DeviceConfigurationOverride.ForcedSize(
+            size = DpSize(
+                width = WIDTH_DP_EXPANDED_LOWER_BOUND.dp,
+                height = WIDTH_DP_EXPANDED_LOWER_BOUND.dp
+            )
+        )
+    ) {
+        content()
+    }
+}
+
+@Composable
+fun DeviceConfigurationOverride.ExpandedWidthCompactHeight(
+    content: @Composable () -> Unit
+) {
+    DeviceConfigurationOverride(
+        override = DeviceConfigurationOverride.ForcedSize(
+            size = DpSize(
+                width = WIDTH_DP_EXPANDED_LOWER_BOUND.dp,
+                height = (HEIGHT_DP_MEDIUM_LOWER_BOUND - 1).dp
+            )
+        )
+    ) {
+        content()
+    }
+}
+
