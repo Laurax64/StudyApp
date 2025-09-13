03b663ee318df0bfbb8f1f80770d9d0613a2df21
lauravoetzsch <LauraVoetzsch@gmail.com>
2025-09-13 12:10:07 +0200

    Make business logic cross-platform by moving the data package into the shared module
    Signed-off-by: lauravoetzsch <LauraVoetzsch@gmail.com>

--- shared/src/commonMain/kotlin/com/example/shared/data/authentication/UserPreferencesRepository.kt
+++ shared/src/commonMain/kotlin/com/example/shared/data/authentication/UserPreferencesRepository.kt
@@ -5,7 +5,6 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 
 data class UserPreferences(
     val currentAuthenticationAlternative: String? = null,
