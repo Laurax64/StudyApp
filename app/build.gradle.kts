76f0d96828ec8898e6323fb8c46155a2bf5b30f8
lauravoetzsch <LauraVoetzsch@gmail.com>
2025-09-27 15:57:40 +0200

    Upgrade GitHub Actions to Java 17 and set sourceCompatibility and targetCompatibility to the current java version in build.gradle.kts
    Signed-off-by: lauravoetzsch <LauraVoetzsch@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.current()
+        targetCompatibility = JavaVersion.current()
     }
     kotlinOptions {
         jvmTarget = "1.8"
